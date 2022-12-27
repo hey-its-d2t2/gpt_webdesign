30467b4cfaca34daf95db80cb689f9e41ca04fdf
DEEPAK SINGH <deepsinghkumar01@gmail.com>
2022-12-27 12:37:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { Article,Brand,CTA,Feature,Navbar} from './components'
+import {Blog,Featurs,Footer,Header,Possibility,WhatGPT3} from './containers'
+import './App.css'
+const App = () => {
+  return (
+    <div className = "App">
+      <div className = "gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+     <Brand />
+     <WhatGPT3 />
+     <Featurs />
+     <Possibility />
+     <CTA />
+     <Blog />
+     <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/brand/import.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.png'
+import shopify from '../../assets/shopify.png'
+import slack from '../../assets/slack.png'
+import dropbox from '../../assets/dropbox.png'
+import atlassian from '../../assets/atlassian.png'
+
+export{
+    google,
+    shopify,
+    slack,
+    dropbox,
+    atlassian
+}
